08e2d531681344867cc2875fc82048541ec59188
Matt Vogel <mainmoniker@googlemail.com>
2021-10-12 18:24:23 -0400

    feat: Convert HTML table to Roam table

---
+++ roam-js/htmlTableToRoam.js
@@ -0,0 +1,64 @@
+{
+    /* Original code by matt vogel */
+    /* Source: https://github.com/8bitgentleman/roam-custom-theme  */
+    /* v1  */
+    // creates a right click menu plugin which will convert a copied HTML table to a roam table
+    // this table was used as reference https://www.w3schools.com/html/html_tables.asp
+
+    async function pasteTable(e) {
+        console.log(e);
+        // get the clipboard text
+        const clipText = await navigator.clipboard.readText();
+        console.log(clipText);
+        // split into rows
+
+        clipRows = clipText.split(String.fromCharCode(13));
+        console.log(clipRows);
+        // split rows into columns
+
+        for (i = 0; i < clipRows.length; i++) {
+            clipRows[i] = clipRows[i].split(String.fromCharCode(9));
+        }
+        console.log(clipRows);
+
+        // 
+        // create the roam table here
+        // 
+        tableUID = window.roamAlphaAPI.util.generateUID();
+        window.roamAlphaAPI.updateBlock({
+            "block": {
+                "uid": e,
+                "string": "{{[[table]]}}"
+            }
+        })
+
+
+        function createRow(e, row) {
+
+            var parentUID = e
+            for (i = 0; i < row.length; i++) {
+                var uid = window.roamAlphaAPI.util.generateUID();
+
+                window.roamAlphaAPI.createBlock({
+                    "location": {
+                        "parent-uid": parentUID,
+                        "order": "last"
+                    },
+                    "block": {
+                        "uid": uid,
+                        "string": row[i]
+                    }
+                })
+                parentUID = uid;
+            }
+        }
+        for (r = 0; r < clipRows.length; r++) {
+            createRow(e, clipRows[r])
+        }
+    }
+
+    roamAlphaAPI.ui.blockContextMenu.addCommand({
+        label: "Paste HTML Table",
+        callback: (e) => pasteTable(e['block-uid'])
+    })
+}
